392be1685bcf697d8dfc1ecd43ee2598dd0ef137
parkjin97339 <parkjin97339@gmail.com>
2024-05-29 16:25:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+from openai import OpenAI
 
-st.write("Hello")
+st.header("비밀번호를 적어주세요.")
+prompt1 = st.text_input(label="비밀번호를 입력하세요.", type="password", key="API")
+st.session_state.key
 
-import openai
-import pandas
+if st.button("입력하기"):
+  client = OpenAI(api_key="{prompt1}")
