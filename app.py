ad1ad62be2446b5a6d3758795ece8cde1a5d1a76
parkjin97339 <parkjin97339@gmail.com>
2024-05-29 16:43:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ prompt1 = st.text_input(label="비밀번호를 입력하세요.", type="password
 
 if st.button("입력하기"):
   st.session_state.name
-  st.write(prompt1)
+  client = OpenAI(api_key=prompt1)
 
 st.header("무엇이든 물어보세요.")
 prompt2 = st.text_input("질문")
-  client = OpenAI(api_key=prompt1)
 
 if st.button("질문하기"):
   response = client.chat.completions.create(
