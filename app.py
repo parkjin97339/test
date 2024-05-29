e8b9c885a1dc555ff70e2151900a7d877ac25794
parkjin97339 <parkjin97339@gmail.com>
2024-05-29 16:27:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ st.session_state.API
 
 if st.button("입력하기"):
   client = OpenAI(api_key="{prompt1}")
+
+st.divider()
+
+st.header("무엇이든 물어보세요.")
+prompt2 = st.text_input("질문")
+
+if st.button("질문하기"):
+  response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "prompt2"}
+    ]
+  )
+  response
+  print(response.choices[0].message.content)
