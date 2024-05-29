d64c7c5859e94e2193f7d93ba1a9f427c863d142
parkjin97339 <parkjin97339@gmail.com>
2024-05-29 16:40:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ st.header("비밀번호를 적어주세요.")
 prompt1 = st.text_input(label="비밀번호를 입력하세요.", type="password")
 
 if st.button("입력하기"):
-  sk = prompt1
+  key1 = prompt1
+  st.print(key1)
   
 st.divider()
 
@@ -13,7 +14,7 @@ st.header("무엇이든 물어보세요.")
 prompt2 = st.text_input("질문")
 
 if st.button("질문하기"):
-  client = OpenAI(api_key="sk");
+  client = OpenAI(api_key="key1");
   response = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
