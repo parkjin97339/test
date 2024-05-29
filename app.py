1dd3159034e7a11afbf1e5811dcf5d1bc999a788
parkjin97339 <parkjin97339@gmail.com>
2024-05-29 16:41:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import streamlit as st
 from openai import OpenAI
 
 st.header("비밀번호를 적어주세요.")
-prompt1 = st.text_input(label="비밀번호를 입력하세요.", type="password")
+prompt1 = st.text_input(label="비밀번호를 입력하세요.", type="password", key="name")
 
 if st.button("입력하기"):
-  key1 = prompt1
-  st.write(key1)
+  st.session_state.name
+  st.write(prompt1)
 
 st.header("무엇이든 물어보세요.")
 prompt2 = st.text_input("질문")
 
 if st.button("질문하기"):
-  client = OpenAI(api_key="key1");
+  client = OpenAI(api_key=prompt1);
   response = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
