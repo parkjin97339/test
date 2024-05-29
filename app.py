21393319cb51200d05bef714299184b4502ee107
parkjin97339 <parkjin97339@gmail.com>
2024-05-29 16:26:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 st.header("비밀번호를 적어주세요.")
 prompt1 = st.text_input(label="비밀번호를 입력하세요.", type="password", key="API")
-st.session_state.key
+st.session_state.API
 
 if st.button("입력하기"):
   client = OpenAI(api_key="{prompt1}")
