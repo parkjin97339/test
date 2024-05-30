13755ecf68ee62de2b54452c71f8528a5ffda46c
parkjin97339 <parkjin97339@gmail.com>
2024-05-30 15:37:14 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,7 +5,11 @@ message1 = st.chat_input("")
 
 if "messages" not in st.session_state: 
  st.session_state.messages = [] 
-
+ 
+for msg in st.session_state.messages: 
+ with st.chat_message(msg["role"]): 
+ st.markdown(msg["content"])
+ 
 st.chat_message("user").markdown(message1)
 
 prompt1 = st.text_input(label="비밀번호를 입력하세요.", type="password", key="name")
