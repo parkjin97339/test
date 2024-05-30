892d7e5957a07e41105ad932bd5b29da4a34dcb2
parkjin97339 <parkjin97339@gmail.com>
2024-05-30 15:46:03 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -3,10 +3,12 @@ import streamlit as st
 st.header("Chat")
 msg = st.chat_input("")
 
-if "messages" not in st.session_state: 
+if "msg" not in st.session_state: 
  st.session_state.messages = [] 
  
 st.chat_message("user").markdown(msg)
 st.session_state.messages.append({"role": "user", "content": msg}) 
 
-
+for msg in st.session_state.messages: 
+ with st.chat_message(msg["role"]): 
+ st.markdown(msg["content"])
