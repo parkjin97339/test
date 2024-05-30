a212ea45c0df632900126c169bf67295af217cc1
parkjin97339 <parkjin97339@gmail.com>
2024-05-30 15:35:52 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -3,6 +3,10 @@ import streamlit as st
 st.header("Chat")
 message1 = st.chat_input("")
 
+if "messages" not in st.session_state: 
+ st.session_state.messages = [] 
+
+st.chat_message("user").markdown(prompt)
 
 prompt1 = st.text_input(label="비밀번호를 입력하세요.", type="password", key="name")
 
