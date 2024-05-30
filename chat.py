7d5f3c75e1bbe2a2535fca52af4d780396bbf5c9
parkjin97339 <parkjin97339@gmail.com>
2024-05-30 15:43:54 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -7,10 +7,6 @@ msg = st.chat_input("")
 if "messages" not in st.session_state: 
  st.session_state.messages = [] 
  
-for msg in st.session_state.messages: 
- with st.chat_message(msg["role"]):
- st.markdown(msg["content"])
- 
 st.chat_message("user").markdown(msg)
 st.session_state.messages.append({"role": "user", "content": prompt}) 
 
