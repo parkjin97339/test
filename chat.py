a7c71677afa3d5fe9be82d1ff0a257abcd3cd0ea
parkjin97339 <parkjin97339@gmail.com>
2024-05-30 16:15:34 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,12 +5,12 @@ from openai import OpenAI
 
 st.header("Chat")
 
+if "messages" not in st.session_state: 
+ st.session_state.messages = []
+
 if msg := st.chat_input("What is up?"): 
  st.chat_message("user").markdown(msg);
  st.session_state.messages.append({"role": "user", "content": msg}) ;
- 
-if "messages" not in st.session_state: 
- st.session_state.messages = []
  
 for msg in st.session_state.messages: 
  with st.chat_message(msg["role"]): 
