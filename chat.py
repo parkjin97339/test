579aebee6161acc8aea59d7bee6497a317c711b5
parkjin97339 <parkjin97339@gmail.com>
2024-05-30 15:58:33 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -17,15 +17,19 @@ if msg := st.chat_input("What is up?"):
 client = OpenAI(api_key="sk-proj-ayiAG3gbanA8KDeThPGcT3BlbkFJJyE8PDgSiDiEkJvfHl4m")
 
 assistant = client.beta.assistants.create(
-  instructions="당신은 부산 광안대교 전문가입니다. 첨부 파일의 정보를 이용해 응답하세요.",
-  model="gpt-3.5-turbo",
-  tools=[{"type": "file_search"}],
-  tool_resources={
-      "file_search":{
-          "vector_store_ids": [vector_store.id]
-      }
-  }
+  name="똑똑한 비서",
+  description="당신은 똑똑한 비서입니다.",
+  model="gpt-3.5-turbo"
 )
 assistant
+
+thread = client.beta.threads.create(
+  messages=[
+    {
+      "role": "user",
+      "content": msg
+    }
+  ]
+)
 
 response = f"Echo: {prompt}"
