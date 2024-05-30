dba2482deb037514a835e3a90a2d582ea6efc3e3
parkjin97339 <parkjin97339@gmail.com>
2024-05-30 16:18:22 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -11,8 +11,18 @@ if "msg" not in st.session_state:
 if msg := st.chat_input("What is up?"): 
  st.chat_message("user").markdown(msg);
  st.session_state.messages.append({"role": "user", "content": msg}) ;
- 
+
+for msg in st.session_state.messages: 
+ with st.chat_message(msg["role"]): 
+ st.markdown(msg["content"])
+
 response = msg
+
+with st.chat_message("assistant"): 
+ st.markdown(response) 
+ st.session_state.messages.append({"role": "assistant", "content": response})
+ 
+
 
 
 
