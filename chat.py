5a270b9472d09a8cbb53fbad1465843e6c09b47d
parkjin97339 <parkjin97339@gmail.com>
2024-05-30 15:59:15 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import openai
 from openai import OpenAI
+
 
 st.header("Chat")
 
@@ -16,14 +18,14 @@ if msg := st.chat_input("What is up?"):
 
 client = OpenAI(api_key="sk-proj-ayiAG3gbanA8KDeThPGcT3BlbkFJJyE8PDgSiDiEkJvfHl4m")
 
-assistant = client.beta.assistants.create(
+assistant = client.assistants.create(
   name="똑똑한 비서",
   description="당신은 똑똑한 비서입니다.",
   model="gpt-3.5-turbo"
 )
 assistant
 
-thread = client.beta.threads.create(
+thread = client.threads.create(
   messages=[
     {
       "role": "user",
