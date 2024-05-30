8bd7ce4c470a2c9600a3a141e2b0ef4e3e7a5a50
parkjin97339 <parkjin97339@gmail.com>
2024-05-30 15:54:49 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,6 +1,5 @@
-import streamlit_chat as st_chat
-from st_chat import message 
 import streamlit as st
+from openai import OpenAI
 
 st.header("Chat")
 
@@ -14,5 +13,19 @@ for msg in st.session_state.messages:
 if msg := st.chat_input("What is up?"): 
  st.chat_message("user").markdown(msg);
  st.session_state.messages.append({"role": "user", "content": msg}) ;
+
+client = OpenAI(api_key="sk-proj-ayiAG3gbanA8KDeThPGcT3BlbkFJJyE8PDgSiDiEkJvfHl4m")
+
+assistant = client.beta.assistants.create(
+  instructions="당신은 부산 광안대교 전문가입니다. 첨부 파일의 정보를 이용해 응답하세요.",
+  model="gpt-3.5-turbo",
+  tools=[{"type": "file_search"}],
+  tool_resources={
+      "file_search":{
+          "vector_store_ids": [vector_store.id]
+      }
+  }
+)
+assistant
 
 response = f"Echo: {prompt}"
