0f4c0bcc4c1fd84a70b4c8908e143b9d33d70dd4
parkjin97339 <parkjin97339@gmail.com>
2024-06-14 22:05:05 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,23 +1,8 @@
 import streamlit as st
-import openai
-import os
-from openai import OpenAI
+from PIL import Image
 
-st.header("Chat")
-
-if "msg" not in st.session_state: 
- st.session_state.messages = []
-
-if msg := st.chat_input("What is up?"): 
- st.chat_message("user").markdown(msg);
- st.session_state.messages.append({"role": "user", "content": msg}) ;
-
-for msg in st.session_state.messages: 
- with st.chat_message(msg["role"]): 
-  st.markdown(msg["content"])
-
- 
-
+img = Image.open('생성AI/세종1관.png')
+st.image(img)
 
 
 
