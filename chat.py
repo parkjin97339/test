b1dd75db87e104d15ef7b87694279475b5255c8a
parkjin97339 <parkjin97339@gmail.com>
2024-06-14 22:09:43 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,7 +1,15 @@
 import streamlit as st
 from PIL import Image
+pip install xlrd
+pip install openpyxl
+pip install pandas
+import pandas as pd
 
 st.image("https://www.pknu.ac.kr/imageView.do?target=campus&cd=B0000024")
+
+file_name = 'test1.xlsx'
+df = pd.read_excel(file_name)
+print(df)
 
 st.header("무엇이든 물어보세요.")
 prompt = st.text_input("질문?")
