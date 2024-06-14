9f51b350ec61efb2f5bd8cb5c225a98dce8412ec
parkjin97339 <parkjin97339@gmail.com>
2024-06-14 22:44:40 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,10 +16,11 @@ if st.button('Assistant 새롭게 생성하기'):
         st.session_state.client = client
     if 'assistant' not in st.session_state:
         st.session_state.assistant = assistant
+    messages = []
         
 st.header("Thread")
 prompt = st.chat_input("메시지를 입력하세요.")
-if prompt := st.chat_input():
+if prompt:
     st.session_state.messages.append({"role": "user", "content": prompt})
     for msg in st.session_state.messages:
         with st.chat_message(msg["role"]):
