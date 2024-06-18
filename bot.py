d247cd86b70c2367f5049661e701618505114453
parkjin97339 <parkjin97339@gmail.com>
2024-06-18 15:42:59 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 
-df = pd.read_excel('https://github.com/parkjin97339/test/blob/main/menu.xlsx', sheet_name='Menu')
+df = pd.read_excel(url='https://github.com/parkjin97339/test/blob/main/menu.xlsx', sheet_name='Menu')
 df
